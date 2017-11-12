b2bb1f28e08d5c41d3fc8dbbd8bf9c81066fa31b
poornimask <poornimakulkarni28@gmail.com>
2017-11-11 21:24:17 -0500

    Update MainController.js

--- resources/js/controllers/MainController.js
+++ resources/js/controllers/MainController.js
@@ -1,4 +1,5 @@
 
+
 app.controller("MainController", ['$scope', function($scope){
   $scope.title = "Top Selling Books of The Month";
   $scope.promo = "Best selling Books";
@@ -16,7 +17,7 @@ app.controller("MainController", ['$scope', function($scope){
       name: 'Program or be Programmed', 
       price: 8, 
       pubdate: new Date('2013', '08', '01'),
-      cover: 'resources/images/image1.jpg',
+      cover: 'resources/images/image2.jpg',
       likes: 0,
       dislikes: 0 
     },
@@ -24,7 +25,7 @@ app.controller("MainController", ['$scope', function($scope){
       name: 'Harry Potter & The Prisoner of Azkaban', 
       price: 11.99, 
       pubdate: new Date('1999', '07', '08'), 
-      cover: 'resources/images/image1.jpg',
+      cover: 'resources/images/image3.jpg',
       likes: 0,
       dislikes: 0
     },
