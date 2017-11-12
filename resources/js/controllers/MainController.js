177af4eaf44d1e18852c1990d4d7db0cbec74958
poornimask <poornimakulkarni28@gmail.com>
2017-11-11 21:28:04 -0500

    Update MainController.js

--- resources/js/controllers/MainController.js
+++ resources/js/controllers/MainController.js
@@ -9,7 +9,7 @@ app.controller("MainController", ['$scope', function($scope){
       name: 'The Book of Trees', 
       price: 19, 
       pubdate: new Date('2014', '03', '08'),
-      cover: 'resources/images/image1.jpg',
+      cover: './resources/images/image1.jpg',
       likes: 0,
       dislikes: 0 
     }, 
@@ -17,7 +17,7 @@ app.controller("MainController", ['$scope', function($scope){
       name: 'Program or be Programmed', 
       price: 8, 
       pubdate: new Date('2013', '08', '01'),
-      cover: 'resources/images/image1.jpg',
+      cover: './resources/images/image1.jpg',
       likes: 0,
       dislikes: 0 
     },
@@ -25,7 +25,7 @@ app.controller("MainController", ['$scope', function($scope){
       name: 'Harry Potter & The Prisoner of Azkaban', 
       price: 11.99, 
       pubdate: new Date('1999', '07', '08'), 
-      cover: 'resources/images/image1.jpg',
+      cover: './resources/images/image1.jpg',
       likes: 0,
       dislikes: 0
     },
@@ -33,7 +33,7 @@ app.controller("MainController", ['$scope', function($scope){
       name: 'Ready Player One', 
       price: 7.99, 
       pubdate: new Date('2011', '08', '16'), 
-      cover: 'resources/images/image1.jpg',
+      cover: './resources/images/image1.jpg',
       likes: 0,
       dislikes: 0
     },
